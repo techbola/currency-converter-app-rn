72ef7252846e492c4fc2f10c0a30a22fa4abe297
Adebola <adebola@poptin.com>
2024-06-07 13:48:47 +0100

    project setup

---
+++ src/components/CurrencyButton.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const CurrencyButton = () => {
+  return (
+    <View>
+      <Text>CurrencyButton</Text>
+    </View>
+  );
+};
+
+export default CurrencyButton;
+
+const styles = StyleSheet.create({});
---
+++ src/constants.ts
@@ -0,0 +1,56 @@
+export const currencyByNaira: Currency[] = [
+  {
+    name: 'DOLLAR',
+    value: 0.00067,
+    flag: '🇺🇸',
+    symbol: '$',
+  },
+  {
+    name: 'EURO',
+    value: 0.00062,
+    flag: '🇪🇺',
+    symbol: '€',
+  },
+  {
+    name: 'POUND',
+    value: 0.00053,
+    flag: '🇬🇧',
+    symbol: '£',
+  },
+  {
+    name: 'RUBLE',
+    value: 0.059,
+    flag: '🇷🇺',
+    symbol: '₽',
+  },
+  {
+    name: 'AUS DOLLAR',
+    value: 0.001,
+    flag: '🇦🇺',
+    symbol: 'A$',
+  },
+  {
+    name: 'CAN DOLLAR',
+    value: 0.00093,
+    flag: '🇨🇦',
+    symbol: 'C$',
+  },
+  {
+    name: 'YEN',
+    value: 0.11,
+    flag: '🇯🇵',
+    symbol: '¥',
+  },
+  {
+    name: 'DINAR',
+    value: 0.00021,
+    flag: '🇰🇼',
+    symbol: 'KD',
+  },
+  {
+    name: 'BITCOIN',
+    value: 0.0000000091,
+    flag: '🎰',
+    symbol: '₿',
+  },
+];
